0be8c64c02051c100336a7e9af729b69d9883ac5
Yu Jing <yujing5b5d@gmail.com>
2017-11-10 14:40:05 -0500

    add multipart form reader

---
+++ multipart_form.go
@@ -0,0 +1,81 @@
+package webeh
+
+import (
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"os"
+	"path/filepath"
+)
+
+var kMaxMemoryParseMultipartForm int64 = 32 << 20
+
+type FileInfo struct {
+	Filename string
+	Header   textproto.MIMEHeader
+	Size     int64
+	Path     string
+	Ext      string
+}
+
+type MultipartFormReader struct {
+	R *http.Request
+}
+
+func NewMultipartFormReader(r *http.Request) (mr *MultipartFormReader) {
+	r.ParseMultipartForm(kMaxMemoryParseMultipartForm)
+	mr = &MultipartFormReader{
+		R: r,
+	}
+	return
+}
+
+func (mr *MultipartFormReader) HandleFile(field string, f func(multipart.File, *multipart.FileHeader) error) (err error) {
+	file, header, e := mr.R.FormFile(field)
+	if e != nil {
+		err = e
+		return
+	} else {
+		defer file.Close()
+		err = f(file, header)
+	}
+	return
+}
+
+func (mr *MultipartFormReader) SaveFile(field string, path, name string) (fileInfo FileInfo, err error) {
+	err = mr.HandleFile(field, func(mf multipart.File, mfh *multipart.FileHeader) (err error) {
+		targetName := name
+		ext := filepath.Ext(mfh.Filename)
+		if len(targetName) > 0 {
+			WebLog(mr.R, "ext:", ext)
+			targetName += ext
+		} else {
+			targetName = mfh.Filename
+		}
+
+		uri := filepath.Join(path, targetName)
+		WebLog(mr.R, "path", uri)
+		os.MkdirAll(path, 0755)
+		if f, e := os.Create(uri); e != nil {
+			err = e
+			return
+		} else {
+			defer f.Close()
+			io.Copy(f, mf)
+			fileInfo.Filename = targetName
+			fileInfo.Ext = ext
+			fileInfo.Path = path
+			fileInfo.Header = mfh.Header
+			fileInfo.Size = mfh.Size
+			f.Sync()
+			return
+		}
+
+	})
+	return
+}
+
+func (mr *MultipartFormReader) GetString(field string) string {
+	return mr.R.FormValue(field)
+}
