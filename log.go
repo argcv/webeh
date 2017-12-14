7e0447a964da63fcde12236900a4b9770acfa6d6
Yu Jing <yujing5b5d@gmail.com>
2017-12-14 14:54:31 -0500

    add printf in logger

--- log.go
+++ log.go
@@ -23,6 +23,12 @@ func WebLog(r *http.Request, v ...interface{}) {
 		fmt.Sprint(v...), r.UserAgent()))
 }
 
+func WebLogf(r *http.Request, f string, v ...interface{}) {
+	logger.Output(2, fmt.Sprintf("%s - \"%s %s\" - \"%v\" - %v",
+		GetUserIp(r), r.Method, r.URL,
+		fmt.Sprintf(f, v...), r.UserAgent()))
+}
+
 func init() {
 	logger = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
 }
